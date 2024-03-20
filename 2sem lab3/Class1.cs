ce4d8d99929121794d11522e40841d8abaa2237e
Katerina <161355341+kkatiefk@users.noreply.github.com>
2024-03-20 13:53:25 +0200

    Add files via upload

---
+++ 2sem lab3/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace _2sem_lab3
+{
+    public class Variables
+    {
+
+        public int Variable1;
+        public int Variable2;
+        public Variables(int Var1, int Var2)
+        {
+            this.Variable1 = Var1;
+            this.Variable2 = Var2;
+        }
+
+        //виводимо
+        public void Write()
+        {
+            Console.WriteLine($"{this.Variable1}, {this.Variable2}");
+        }
+
+        //змінаа значень
+        public void Change(int NewVar1, int NewVar2)
+        {
+            this.Variable1 = NewVar1;
+            this.Variable2 = NewVar2;
+        }
+
+        //cума
+        public int Sum()
+        {
+            return this.Variable1 + this.Variable2;
+        }
+
+        //макс
+        public int Max()
+        { 
+            return Math.Max( this.Variable1, this.Variable2);
+        }
+
+        //сереал в json
+        public void SaveToJson(string fileName)
+        {
+            string json = JsonConvert.SerializeObject(this);
+            File.WriteAllText(fileName, json);
+        }
+
+        //десер з json
+        public static Variables LoadFromJson(string fileName)
+        {
+            string json = File.ReadAllText(fileName);
+            Variables loadedVariables = JsonConvert.DeserializeObject<Variables>(json);
+            return loadedVariables;
+        }
+    }
+}
---
+++ 2sem lab3/Program.cs
@@ -0,0 +1,29 @@
+﻿using _2sem_lab3;
+Variables v = new Variables(2, 5);
+Console.WriteLine("Two variables:");
+v.Write();
+
+v.Change(12, 100);
+Console.WriteLine("\nChanged variables:");
+v.Write();
+
+Console.WriteLine("\nThe sum of this two variables: " + v.Sum());
+
+
+Console.WriteLine("\nThe maximum of this two variables: " + v.Max());
+
+string filePath = "variables.json";
+v.SaveToJson(filePath);
+Console.WriteLine($"\nObject saved to {filePath}");
+
+Console.WriteLine("\nContent of saved JSON file:");
+string jsonContent = File.ReadAllText(filePath);
+Console.WriteLine(jsonContent);
+
+Variables loadedV = Variables.LoadFromJson(filePath);
+Console.WriteLine("\nLoaded variables from JSON:");
+loadedV.Write();
+
+Console.ReadKey();
+
+
